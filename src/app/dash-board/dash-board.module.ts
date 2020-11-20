0b8c69f18b4df2332a321df48df128b2395a5858
Isac Bueno <isac.bueno@aluno.unip.br>
2020-11-20 11:50:47 -0300

    Dashboard inicial

---
+++ src/app/dash-board/dash-board.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DashBoardPageRoutingModule } from './dash-board-routing.module';
+
+import { DashBoardPage } from './dash-board.page';
+import { ComponentModule } from '../component/component.module';
+import { HeaderComponent } from '../component/header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DashBoardPageRoutingModule,
+    ComponentModule
+  ],
+  declarations: [DashBoardPage,HeaderComponent]
+})
+export class DashBoardPageModule {}
---
+++ src/app/dash-board/dash-board.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-dash-board',
+  templateUrl: './dash-board.page.html',
+  styleUrls: ['./dash-board.page.scss'],
+})
+export class DashBoardPage implements OnInit {
+
+  public folder: string;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+}
