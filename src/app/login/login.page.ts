57c5bebadc6335303076dbd8b494a10b79fe776e
Isac Bueno <isac.bueno@aluno.unip.br>
2020-11-21 10:36:25 -0300

    Configuraçao de formulario, e enviando requisicao

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AuthUsuario } from '../models/AuthUsuario';
+import { AuthService } from '../servicos/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor( 
+    private formBuilder: FormBuilder,
+    public loadingController: LoadingController,
+    public auth:AuthService,private router: Router,public toastController: ToastController) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      userName: [' ', Validators.required],
+      password: [' ', Validators.required],
+    });
+  }
+  async Entrar() {
+    // this.router.navigate(['/home']);
+    console.log('vai se autenticar');
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+    console.log(userName,password);
+    
+    this.presentLoading();
+     (await this.auth.autenticacao(userName,password)).subscribe(
+      (data)=>{
+        this.ConsultaSituacao(data)
+      },err=>{
+       
+        this.ToastErro();
+
+      }
+    )
+    
+  
+    // Aqui eu venho e pego as informaçoes do usuario para validar para qual
+    // pagina ele vai
+    
+  }
+  async ToastErro() {
+    const toast = await this.toastController.create({
+      message: 'login ou senha errado',
+      position: 'middle',
+      duration: 5000,
+      color: 'danger',
+      buttons: [
+        {
+          handler: () => {
+            this.loginForm.reset();
+          },
+          text: 'ok',
+          role: 'cancel',
+          
+        },
+      ],
+    });
+    toast.present();
+  }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      spinner: 'bubbles',
+      message: 'Aguarde...',
+      duration: 60000,
+    });
+    await loading.present();
+
+  }
+  ConsultaSituacao(data) {
+    let dataUser: AuthUsuario = data;
+    debugger
+    if (dataUser.codPerfil == 2) {
+      // this.native.setItem('session_storange', dataUser);
+      this.router.navigate(['/dash-board' ]);
+    } else {
+      this.router.navigate(['/dadosviagem']);
+      // this.native.setItem('session_storange', dataUser);
+      // this.router.navigate([
+      //   '/home',
+      //   dataUser.usuariosCodUsuario,
+      //   dataUser.clientesCodCliente,
+      // ]);
+    }
   }
 
 }
