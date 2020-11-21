985535ff741f5e2ef0aff5a741d74c60b9159f5e
Isac Bueno <isac.bueno@aluno.unip.br>
2020-11-21 10:27:39 -0300

    Modulos para formulario

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import{ ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
